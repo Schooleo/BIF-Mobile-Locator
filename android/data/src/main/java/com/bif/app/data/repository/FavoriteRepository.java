a4b7a098caa239c86c4241146ce79fd8e4a226de
KwanTheAsian <bienxuanan@gmail.com>
2026-03-01 07:20:48 +0700

    [Favorite][Data] Implement add/update/remove FavoriteEntity in FavoriteRepository, IFavoriteRepository

--- android/data/src/main/java/com/bif/app/data/repository/FavoriteRepository.java
+++ android/data/src/main/java/com/bif/app/data/repository/FavoriteRepository.java
@@ -1,29 +1,52 @@
 package com.bif.app.data.repository;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
+
+import com.bif.app.data.mapper.FavoriteMapper;
+import com.bif.app.data.source.local.FavoriteDao;
 import com.bif.app.domain.model.Favorite;
 import com.bif.app.domain.repository.IFavoriteRepository;
-import androidx.lifecycle.LiveData;
+
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import javax.inject.Inject;
 
 public class FavoriteRepository implements IFavoriteRepository {
 
+    private final FavoriteDao favoriteDao;
+    private final ExecutorService executorService;
+
     @Inject
-    public FavoriteRepository() {
+    public FavoriteRepository(FavoriteDao favoriteDao) {
+        this.favoriteDao = favoriteDao;
+        this.executorService = Executors.newFixedThreadPool(4);
     }
 
     @Override
     public LiveData<List<Favorite>> getAllFavorites() {
-        return null;
+        return Transformations.map(favoriteDao.getAll(), FavoriteMapper::toDomainList);
     }
 
     @Override
     public void addFavorite(Favorite favorite) {
+        executorService.execute(() -> favoriteDao.insert(FavoriteMapper.toEntity(favorite)));
+    }
 
+    @Override
+    public void updateFavorite(Favorite favorite) {
+        executorService.execute(() -> favoriteDao.update(FavoriteMapper.toEntity(favorite)));
+    }
+
+    @Override
+    public void updateAllFavorites(List<Favorite> favorites) {
+        executorService.execute(() -> favoriteDao.updateAll(FavoriteMapper.toEntityList(favorites)));
     }
 
     @Override
     public void deleteFavorite(Favorite favorite) {
-
+        executorService.execute(() -> favoriteDao.delete(FavoriteMapper.toEntity(favorite)));
     }
 }
--- android/domain/src/main/java/com/bif/app/domain/repository/IFavoriteRepository.java
+++ android/domain/src/main/java/com/bif/app/domain/repository/IFavoriteRepository.java
@@ -9,5 +9,7 @@ import java.util.List;
 public interface IFavoriteRepository {
     LiveData<List<Favorite>> getAllFavorites();
     void addFavorite(Favorite favorite);
+    void updateFavorite(Favorite favorite);
+    void updateAllFavorites(List<Favorite> favorites);
     void deleteFavorite(Favorite favorite);
 }
