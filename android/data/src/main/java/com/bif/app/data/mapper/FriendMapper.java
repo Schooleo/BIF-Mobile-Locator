a96f031ec3f15cb504992e6957ee22cf42fd78e5
vnquy126904 <ngocquyvan2005@gmail.com>
2026-03-15 01:44:00 +0700

    [Social][Mapper] add FriendMapper for entity-domain conversion with tests

---
+++ android/data/src/main/java/com/bif/app/data/mapper/FriendMapper.java
@@ -0,0 +1,32 @@
+package com.bif.app.data.mapper;
+
+import com.bif.app.data.source.local.entity.FriendEntity;
+import com.bif.app.domain.model.Friend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendMapper {
+    public static Friend toDomain(FriendEntity entity) {
+        return new Friend(entity.name, entity.avatarLetter, entity.avatarColor, entity.isOnline);
+    }
+
+    public static FriendEntity toEntity(Friend domain) {
+        FriendEntity entity = new FriendEntity();
+        entity.name = domain.getName();
+        entity.avatarLetter = domain.getAvatarLetter();
+        entity.avatarColor = domain.getAvatarColor();
+        entity.isOnline = domain.isOnline();
+        return entity;
+    }
+
+    public static List<Friend> toDomainList(List<FriendEntity> entities) {
+        List<Friend> list = new ArrayList<>();
+        if (entities != null) {
+            for (FriendEntity entity : entities) {
+                list.add(toDomain(entity));
+            }
+        }
+        return list;
+    }
+}
